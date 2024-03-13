f7553c7f19d82738d51f79305054db5dc98f1d3a
madamczak <adamczak.mateusz@gmail.com>
2024-03-13 19:36:29 +0100

    refactor citiesscrapper.py to use nominatim and geodms functions

--- citiesscrapper.py
+++ citiesscrapper.py
@@ -9,22 +9,34 @@ def get_soup(link):
     return soup
 
 
+def get_geo_dms_coords(link):
+    soup = get_soup(link)
+    geo_dms = soup.findAll('span', {'class': 'geo-dms'})
+
+    if len(geo_dms) > 2:
+        lat, lon = geo_dms[1].text.replace('\xa0', " ").replace(",", ".").split(" ")
+        return float(lat), float(lon)
+
+    return 0.0, 0.0
+
+
+def get_nominatim_coords(city_name):
+    searchlink = f"https://nominatim.openstreetmap.org/search?q={city_name}+Polska&format=json&addressdetails=1&limit=1"
+    nominatim_respone = requests.get(searchlink)
+    city_list = json.loads(nominatim_respone.text)
+    city_dictionary = city_list[0]
+    return float(city_dictionary.get("lat")), float(city_dictionary.get('lon'))
+
+
 def get_partnering_cities_locations_with_nominatim(wiki_link):
     cities_dictionary = {}
     soup = get_soup(wiki_link)
-
     all_h2 = soup.findAll("h2")[1:]
 
     for h2 in all_h2:
         city_name = h2.text.replace("[edytuj | edytuj kod]", "")
-        searchlink = f"https://nominatim.openstreetmap.org/search?q={city_name}+Polska&format=json&addressdetails=1&limit=1"
-
-        nominatim_respone = requests.get(searchlink)
-        city_list = json.loads(nominatim_respone.text)
-        city_dictionary = city_list[0]
-
-        # cities_dictionary[city_name] = (city_dictionary.get("lat"), city_dictionary.get('lon'))
-        cities_dictionary.update({city_name: (float(city_dictionary.get("lat")), float(city_dictionary.get('lon')))})
+        lat, lon = get_nominatim_coords(city_name)
+        cities_dictionary.update({city_name: (lat, lon)})
 
     return cities_dictionary
 
@@ -41,15 +53,9 @@ def get_partnering_cities_locations_with_geo_dms(wiki_link):
         city_name = city_a_tag.text
         city_href = city_a_tag['href']
         city_link = f'https://pl.wikipedia.org{city_href}'
+        lat, lon = get_geo_dms_coords(city_link)
 
-        city_link_response = requests.get(city_link)
-        city_soup = BeautifulSoup(city_link_response.text, features="html.parser")
-        geo_dms = city_soup.findAll('span', {'class': 'geo-dms'})
-        decimal_degrees = geo_dms[1]
-        lat, lon = decimal_degrees.text.replace('\xa0', " ").replace(",", ".").split(" ")
-        lat_float = float(lat)
-        lon_float = float(lon)
-        another_cities_dictionary.update({city_name: (lat_float, lon_float)})
+        another_cities_dictionary.update({city_name: (lat, lon)})
 
     return another_cities_dictionary
 
@@ -64,6 +70,8 @@ def get_cities_with_partnering_cities(wiki_link):
 
     for table, h2 in zip(all_tables, all_h2):
         main_city_name = h2.text.replace("[edytuj | edytuj kod]", "")
+        main_city_lat, main_city_lon = get_nominatim_coords(main_city_name)
+
         partnering_cities_dict = {}
         all_rows = table.findAll("tr")
         for row in all_rows[1:]:
@@ -72,9 +80,13 @@ def get_cities_with_partnering_cities(wiki_link):
                 city_cell = all_td[0]
                 city_name = city_cell.text.replace('\xa0', '')
                 city_link = "https://pl.wikipedia.org" + city_cell.findAll('a')[0]['href']
-                print(city_name, city_link)
-                partnering_cities_dict[city_name] = city_link
+                city_lat, city_lon = get_geo_dms_coords(city_link)
 
-        cities_with_parnering_cities[main_city_name] = partnering_cities_dict
+                #partnering_cities_dict[city_name] = city_link
+                partnering_cities_dict[city_name] = [city_lat, city_lon]
+
+        #cities_with_parnering_cities[main_city_name] = partnering_cities_dict
+        cities_with_parnering_cities[main_city_name] = {'coords': [main_city_lat, main_city_lon],
+                                                        'partnering_cities': partnering_cities_dict}
 
     return cities_with_parnering_cities
--- draw_map.py
+++ draw_map.py
@@ -1,20 +1,24 @@
 import json
 import folium
 
-
 mymap = folium.Map(location=[52.2337172, 21.0714322], zoom_start=7)
-
 
 
 with open("geo_dms.json") as nm_fl:
     cities = json.load(nm_fl)
 
 
-
 for city in cities:
     folium.Marker(location=cities.get(city), popup=city).add_to(mymap)
-
     folium.PolyLine(locations=[cities.get(city), [52.2337172, 21.0714322]], color='blue').add_to(mymap)
 
 
 mymap.save("map.html")
+
+# napisać funkcję która narysuje czerwony marker dla miasta głównego, niebieskie markery dla miast partnerskich
+# i linie łączące te miasta
+
+# petla for przez miasta główne
+# pobranie koordynatów i listy miast partnerskich
+# pętla przez miasta partnerskie
+# pobranie koordynatów miasta partnerskiego
